29e280d2348ab8a5b3c3889bc3175e9334a34fd1
David Anthony <djanthony@gmail.com>
2024-02-29 11:03:58 -0600

    Bumping changelog and version numbers for release (#208)

--- src/main/webapp/resources/js/views/AboutWindow.js
+++ src/main/webapp/resources/js/views/AboutWindow.js
@@ -35,10 +35,10 @@ Ext.define('BagDatabase.views.AboutWindow', {
     layout: 'fit',
     bodyPadding: 5,
     constrainHeader: true,
-    html: "<h2>Bag Database 3.5.1</h2>" +
+    html: "<h2>Bag Database 3.5.2</h2>" +
         "<p>Documentation: <a href='https://swri-robotics.github.io/bag-database/'>https://swri-robotics.github.io/bag-database/</a></p>" +
         "<p>Source Code: <a href='https://github.com/swri-robotics/bag-database'>https://github.com/swri-robotics/bag-database</a></p>" +
-        "<p>Copyright 2015-2020 Southwest Research Institute</p>" +
+        "<p>Copyright 2015-2024 Southwest Research Institute</p>" +
         "<br>" +
         "<p>Icons provided by <a href='http://www.famfamfam.com/lab/icons/silk/'>http://www.famfamfam.com/lab/icons/silk/</a></p>" +
         "<p>A number of open source libraries were used in the creation of this software;<br>" +
